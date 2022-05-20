579a53280195e5939911b44e8376199e5d6d2d57
yujian <ok159329@163.com>
2022-05-20 19:36:36 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h2> 我是views</h2>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// createApp({
+//     el:'#app',
+//     render(h) {
+//         return h(App)
+//     }
+// })
 createApp(App).mount('#app')
